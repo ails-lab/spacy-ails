1ee65e0e0cc1f5162729d6ac4a9bb62a8183b4df
Jason Liartis <g.liartis@gmail.com>
2022-11-06 17:41:37 +0200

    changed logs from stdout to log file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from typing import Union
-from datetime import datetime
+from datetime import datetime, date
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from src.lemmatization import lemmatize
+
+
+LOGS_FN = '/var/log/containers/docker-spacy-ails'
 
 
 # defines the fields of the lemmatization request
@@ -25,14 +28,17 @@ async def lemmatize_call(body: LemmaRequest) -> dict[str, list[dict]]:
     """
     Lemmatization call. See lemmatize.py for an explanation of the request fields.
     @return: A json object containing the lemmas. The lemmas are contained in the field `lemmas` as
-             a list. Each item of the list is a json object containing the lemma in the field
-             `lemma`. If `trace` is set to `True` then the object also contains the fields `start`
-             and `end`. See lemmatize.py for the meaning of those fields.
+    a list. Each item of the list is a json object containing the lemma in the field
+    `lemma`. If `trace` is set to `True` then the object also contains the fields `start`
+    and `end`. See lemmatize.py for the meaning of those fields.
     """
-    print('Request received:')
-    print(body)
-    t_start = datetime.now()
-    print('Starting request processing: ', str(t_start))
+    today = date.today()
+    with open(LOGS_FN + str(today) + '.log', 'a') as fp:
+        print('Request received:', file=fp)
+        print(body, file=fp)
+        t_start = datetime.now()
+        print('Starting request processing: ', str(t_start), file=fp)
+
     try:
         if body.trace is not None and body.trace == "True":
             lemmas = lemmatize(body.s, body.lib, body.model, include_trace=True)
@@ -45,8 +51,9 @@ async def lemmatize_call(body: LemmaRequest) -> dict[str, list[dict]]:
         raise HTTPException(status_code=400, detail=str(err))
     t_end = datetime.now()
     duration = t_end - t_start
-    print('Request processed: ', str(t_end))
-    print('Duration: ', str(duration))
-    print('Lemma count: {}'.format(len(lemmas)))
-    print('Time per lemma: {:.3f}s'.format(duration.total_seconds()/len(lemmas)))
+    with open(LOGS_FN + str(today) + '.log', 'a') as fp:
+        print('Request processed: ', str(t_end), file=fp)
+        print('Duration: ', str(duration), file=fp)
+        print('Lemma count: {}'.format(len(lemmas)), file=fp)
+        print('Time per lemma: {:.3f}s'.format(duration.total_seconds()/len(lemmas)), file=fp)
     return {"lemmas": lemmas}
